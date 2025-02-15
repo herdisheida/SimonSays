b8c3bd0d50ca58690d97a607eaafb69e51cfe419
Herdis Heiõa Jing Gujohnsen <herdisheida@gmail.com>
2025-02-15 17:50:10 +0000

    update key press --- prettier code

--- assignment2_starter_pack/app.js
+++ assignment2_starter_pack/app.js
@@ -4,7 +4,7 @@
 
 
 // give each pad a unique keyboard key
-const padKeys = {
+const keyToPad = {
     q: "pad-red",
     w: "pad-yellow",
     a: "pad-green",
@@ -13,7 +13,7 @@ const padKeys = {
 let padSequence = [] // TODO: vtk hvort ég ætli að initaliza þetta hér..
 let level = 1; // TODO: starts at lvl 1
 let padSeqIndex = 0
-
+let isKeyboardEnabled = false
 
 // const synth = new Tone.Synth().toDestination(); // TEST
 
@@ -22,46 +22,37 @@ let padSeqIndex = 0
 
 
 
-// when page is reloaded or opened for the first time
+// idle-state, all buttans except the start-btn are disabled
 const resetGame = () => {
-    padSequence = []
-    level = 1
-
-    // disable all buttons except the start-btn
+    padSequence = [];
+    level = 1;
     disableButtons();
     document.getElementById("start-btn").disabled = false;
     console.log("Game reset to idle state"); // DELETE console
 }
 
+// all buttons are enabled and the start-btn is disabled
 const startGame = () => {
-    // enable all buttons and disable the start-btn
     enableButtons();
     document.getElementById("start-btn").disabled = true;
 
-    addToSequence() // TEST
+    addToSequence();
+    playSequence();
 
-    // put focus on a pad, in order for keyboard to work simulatnioutsly
-    document.getElementById("pad-red").focus() // TODO: FIX THIS UGLY THING -- it works but ugly
-
-    console.log("Game has started") // DELETE console
 
 // TODO The frontend plays the sequence by lighting up pads and playing sounds with reasonable interval.
 // TODO The tone style that is played must be the one that is selected in the dropdown menu.
 }
 
 const disableButtons = () => {
-    // disable all button elements
     const allButtons = document.querySelectorAll("button");
     allButtons.forEach(button => {button.disabled = true});
-    // disable keyboard
-    document.onkeydown = () => {return false};
 }
 const enableButtons = () => {
-    // enable all button elements
     const allButtons = document.querySelectorAll("button");
     allButtons.forEach(button => {button.disabled = false});
-    // enable keyboard
-    document.onkeydown = () => {return true};
+    isKeyboardEnabled = true;
+
 }
 
 
@@ -71,14 +62,14 @@ const pressPad = (padId) => {
     // make sound
     // save pressPad --- compare it with padSequences
     // We initialise the synthesiser
-    playNote();
+    playSound();
     padSeqIndex += 1;
 
     checkMatch(padId); // check if pad press was correct
     addToSequence() // add if necesary
 }
 
-const playNote = () => {
+const playSound = () => {
     // get the select elements
     const sounds = document.getElementById("sound-select");
     // get the selected sound
@@ -102,22 +93,19 @@ const checkMatch = (padId) => {
 }
 
 const addToSequence = () => { // TODO: kannski ekkki fall - lookar stupid
-    if (padSequence.length === padSeqIndex) {
-        padSequence.push(getRandomPad());
-        playSequence() // play new sequence
-    };
+    if (padSequence.length === padSeqIndex) {padSequence.push(getRandomPad())};
 }
     // play the pad-sequence
 const playSequence = (e) => {
     console.log(padSequence)
     // padSequence.forEach(padId => {}) 
 
-    console.log("Replay button was pressed");
+    console.log("play sequence was pressed");
 };
 
 const getRandomPad = () => {
     // get values from object
-    const padValues = Object.values(padKeys);
+    const padValues = Object.values(keyToPad);
     // randomize index
     let i = Math.floor(Math.random() * padValues.length);
     // get and return a random pad from the index
@@ -126,25 +114,41 @@ const getRandomPad = () => {
 }
 
 
-
-
-
-const keyPressPad = (e) => {
-     // find corresponding pad-id from key
-    const padId = padKeys [e.key];
-    if (padId) { // if valid key is pressed
-        const pad = document.getElementById(padId);
-        animatePadPress(padId);
+// play tune when pressed and animate pad
+document.addEventListener("keyup", (e) => {
+    if (! isKeyboardEnabled) {return};
+    padId = keyToPad[e.key]
+    if (padId) {
         pressPad(padId);
-    };
-}
-const animatePadPress = (padId) => {
-    const pad = document.getElementById(padId);
-    pad.classList.add("clickKey"); // enables pad:active
-    setTimeout(() => {
-        pad.classList.remove("clickKey"); // disables pad:active
-    }, 190); // animate press for 190ms
-}
+        document.getElementById(padId).classList.remove("clickKey");
+        console.log("GOING THROUGH")
+    }
+});
+
+// turns pad back to original look
+document.addEventListener("keydown", (e) => {
+    if (! isKeyboardEnabled) {return};
+    if (keyToPad[e.key]) {
+        document.getElementById(keyToPad[e.key]).classList.add("clickKey")
+    }
+});
+
+// const keyPressPad = (e) => {
+//      // find corresponding pad-id from key
+//     const padId = keyToPad [e.key];
+//     if (padId) { // if valid key is pressed
+//         const pad = document.getElementById(padId);
+//         animatePadPress(padId);
+//         pressPad(padId);
+//     };
+// }
+// const animatePadPress = (padId) => {
+//     const pad = document.getElementById(padId);
+//     pad.classList.add("clickKey"); // enables pad:active
+//     setTimeout(() => {
+//         pad.classList.remove("clickKey"); // disables pad:active
+//     }, 190); // animate press for 190ms
+// }
 
 
 // user changes pad-press-sound
